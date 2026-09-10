b31394a3d94f7f0bf8e16e6d14527721a5325b03
cooperjane549-cmd <cooperjane549@gmail.com>
2026-09-10 13:31:07 +0300

    Fix method name from meCreateState to createState

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class ClientHomePage extends StatefulWidget {
   const ClientHomePage({super.key});
 
   @override
-  State<ClientHomePage> meCreateState() => _ClientHomePageState();
+  State<ClientHomePage> createState() => _ClientHomePageState();
 }
 
 class _ClientHomePageState extends State<ClientHomePage> {
